cbabdee724a1ba0215197f00521781696c75996d
Marcos Alanya Pacheco <103856433+MarcosAlanya19@users.noreply.github.com>
2023-11-08 01:07:21 -0500

    CONFIG: Obrero.java
    La clase no debe ser abstract y asimismo no debe tener un id

--- src/main/java/com/mph/demo/entity/Obrero.java
+++ src/main/java/com/mph/demo/entity/Obrero.java
@@ -17,8 +17,7 @@ import lombok.Setter;
 @Entity
 @Table(name="obrero")
 @NoArgsConstructor
-public abstract class Obrero extends BaseEntity { 
-    private int idObrero;
+public class Obrero extends BaseEntity { 
     private String nombre;
     private String apellidos;
     private String telefono;
@@ -27,5 +26,4 @@ public abstract class Obrero extends BaseEntity {
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "proyecto_id")
     private Proyecto proyecto;
-
 }
