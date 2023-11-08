ec203a70ef6023d844812d194ccb5ab81c7374df
lewis <luisangelcalleallcca@gmail.com>
2023-11-08 01:49:40 -0500

    este es un avance3

--- src/main/java/com/mph/demo/controller/ObreroController.java
+++ src/main/java/com/mph/demo/controller/ObreroController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 import com.mph.demo.entity.Obrero;
 import com.mph.demo.service.ObreroService;
-import com.mph.demo.shared.BaseService;
+
 
 public class ObreroController {
   @Autowired
@@ -33,10 +32,7 @@ public class ObreroController {
     return obrero.getById(id);
   }
 
-  @PutMapping("{id}")
-  public Obrero putCaseById(@PathVariable Long id, @RequestBody EvidenceEntity evidence) {
-    return obrero.updateEvidence(id, evidence);
-  }
+  
 
   @DeleteMapping("{id}")
   public void deleteCaseById(@PathVariable Long id) {
--- src/main/java/com/mph/demo/controller/UsuarioController.java
+++ src/main/java/com/mph/demo/controller/UsuarioController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import com.mph.demo.entity.Obrero;
+
+
 import com.mph.demo.entity.Usuario;
-import com.mph.demo.service.ObreroService;
+
 import com.mph.demo.service.UsuarioService;
 
 public class UsuarioController {
@@ -34,10 +33,7 @@ public class UsuarioController {
     return usuario.getById(id);
   }
 
-  @PutMapping("{id}")
-  public Usuario putCaseById(@PathVariable Long id, @RequestBody EvidenceEntity evidence) {
-    return usuario.updateEvidence(id, evidence);
-  }
+
 
   @DeleteMapping("{id}")
   public void deleteCaseById(@PathVariable Long id) {
